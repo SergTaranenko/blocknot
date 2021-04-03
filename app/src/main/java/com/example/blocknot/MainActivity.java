8350b6b20320b2517c39855a13515d8b08b972c5
SergTaranenko <rates-13@inbox.ru>
2021-04-03 15:39:09 +0300

    11

--- app/src/main/java/com/example/blocknot/MainActivity.java
+++ app/src/main/java/com/example/blocknot/MainActivity.java
@@ -1,107 +1,83 @@
 package com.example.blocknot;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentTransaction;
+import java.util.Objects;
 
+import com.example.blocknot.observe.Publisher;
 import com.example.blocknot.ui.ListOfNotesFragment;
-import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
+public class MainActivity extends AppCompatActivity {
+        private com.example.blocknot.Navigation navigation;
+        private Publisher publisher = new Publisher();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.list_of_notes_fragment_container, new ListOfNotesFragment());
-        fragmentTransaction.commit();
-        Toolbar toolbar = initToolbar();
-        initDrawer(toolbar);
-    }
-
-    private void initDrawer(Toolbar toolbar) {
-        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar,
-                R.string.drawer_open,
-                R.string.drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(item -> {
-            int id = item.getItemId();
-            if (navigateMenu(id)) {
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
-            }
-            return false;
-        });
-    }
-
-    private boolean navigateMenu(int id) {
-        switch (id) {
-            case R.id.nav_settings:
-                Toast.makeText(this, getResources().getString(R.string.settings), Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.nav_about:
-                Toast.makeText(this, getResources().getString(R.string.about), Toast.LENGTH_LONG).show();
-                return true;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            navigation = new com.example.blocknot.Navigation(getSupportFragmentManager());
+            initToolbar();
+            getNavigation().addFragment(ListOfNotesFragment.newInstance(), false);
         }
-        return false;
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        MenuItem search = menu.findItem(R.id.menu_search);
-        SearchView searchView = (SearchView) search.getActionView();
-        MenuItem sort = menu.findItem(R.id.menu_sort);
-        MenuItem addPhoto = menu.findItem(R.id.menu_add_photo);
-        MenuItem send = menu.findItem(R.id.menu_send);
-        send.setOnMenuItemClickListener(this);
-        addPhoto.setOnMenuItemClickListener(this);
-        sort.setOnMenuItemClickListener(this);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Snackbar.make(findViewById(R.id.drawer_layout), query, Snackbar.LENGTH_SHORT).show();
+        private void initToolbar() {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        @Override
+        public boolean onSupportNavigateUp() {
+            onBackPressed();
+            return true;
+        }
+
+        public com.example.blocknot.Navigation getNavigation() {
+            return navigation;
+        }
+
+        public Publisher getPublisher() {
+            return publisher;
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            getMenuInflater().inflate(R.menu.main_menu, menu);
+            MenuItem search = menu.findItem(R.id.menu_search);
+            SearchView searchText = (SearchView) search.getActionView();
+            searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Toast.makeText(com.example.blocknot.MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return true;
+                }
+            });
+            MenuItem sort = menu.findItem(R.id.menu_sort);
+            sort.setOnMenuItemClickListener(item -> {
+                Toast.makeText(com.example.blocknot.MainActivity.this, R.string.menu_sort, Toast.LENGTH_SHORT).show();
                 return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
+            });
+            MenuItem send = menu.findItem(R.id.menu_send);
+            send.setOnMenuItemClickListener(item -> {
+                Toast.makeText(com.example.blocknot.MainActivity.this, R.string.menu_send, Toast.LENGTH_SHORT).show();
                 return true;
-            }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean onMenuItemClick(MenuItem item) {
-        Snackbar.make(findViewById(R.id.drawer_layout), item.getTitle().toString(), Snackbar.LENGTH_SHORT).show();
-        return false;
-    }
-
-    private boolean checkLandScapeOrientation() {
-        return getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-    }
-
-    private Toolbar initToolbar() {
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        return toolbar;
-    }
+            });
+            MenuItem addPhoto = menu.findItem(R.id.menu_add_photo);
+            addPhoto.setOnMenuItemClickListener(item -> {
+                Toast.makeText(com.example.blocknot.MainActivity.this, R.string.menu_add_photo, Toast.LENGTH_SHORT).show();
+                return true;
+            });
+            return true;
+        }
 }
